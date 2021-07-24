4a760f1f301e38aec11e3ecf65a0716a144a0ca6
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-07-24 17:46:18 +0200

    test: Bills.js 68%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,84 @@
 import { screen } from "@testing-library/dom"
+import  userEvent  from '@testing-library/user-event'
+import { ROUTES } from "../constants/routes"
+import Bills from "../containers/Bills.js"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+
+describe("GIVEN i am connected as an employee", () => {
+  describe("WHEN i am on bills Page and there are no bills", () => {
+    test("THEN bills should be empty", () => {
+      const html = BillsUI({ data: [] });
+      document.body.innerHTML = html;
+
+      const eyeIconElt = screen.queryByTestId("icon-eye")
+      expect(eyeIconElt).toBeNull()
     })
-    test("Then bills should be ordered from earliest to latest", () => {
+  })
+  describe("WHEN i am on bills Page and there are bills", () => {
+    test("THEN bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
+
       const dates = Array.from(document.body.querySelectorAll("#data-table tbody>tr>td:nth-child(3)")).map((a) => a.innerHTML);
+
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+    describe("WHEN I click on the button for create a new bill", () => {
+      test("THEN the add billing note page should open", () => {
+        const html = BillsUI({ data: bills })
+        document.body.innerHTML = html
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        const billsOnUI = new Bills({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage
+        })
+        const handleClickNewBill = jest.fn((e) => billsOnUI.handleClickNewBill(e)) 
+        const newBillBtnElt = screen.getByTestId('btn-new-bill')
+
+        newBillBtnElt.addEventListener('click', handleClickNewBill)
+        userEvent.click(newBillBtnElt)
+
+        expect(handleClickNewBill).toHaveBeenCalled()
+      })
+    })
+    describe('WHEN I click on a bill icon eye', () => {
+      test('THEN a modal should open', () => {
+        const html = BillsUI({ data: bills })
+        document.body.innerHTML = html
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        const billsOnUI = new Bills({
+            document,
+            onNavigate,
+            firestore: null,
+            localStorage: window.localStorage,
+        })
+
+        $.fn.modal = jest.fn();
+
+        const iconEyeElt = screen.getAllByTestId("icon-eye")[0];
+        const handleClickIconEye = jest.fn(() => billsOnUI.handleClickIconEye(iconEyeElt));
+
+        iconEyeElt.addEventListener("click", handleClickIconEye);
+        userEvent.click(iconEyeElt)
+        expect(handleClickIconEye).toHaveBeenCalled();
+
+        const modalElt = document.getElementById("modaleFile");
+        expect(modalElt).toBeTruthy();
+      })
     })
   })
 })
