ba54cc9122d1a2e9f62d6aa8466aa27996c3d00b
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-07-30 17:04:57 +0200

    test: NewBill.js 88% with addition of a method to manage file change in storage

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -4,6 +4,7 @@ import NewBill from "../containers/NewBill.js"
 import { ROUTES } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 
+// Setup for test
 Object.defineProperty(window, 'localStorage', { 
   value: localStorageMock 
 })
@@ -11,10 +12,15 @@ Object.defineProperty(window, 'localStorage', {
 window.localStorage.setItem(
   'user',
   JSON.stringify({
-    type: 'Employee',
+    type: 'Employee'
   })
 )
 
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname });
+};
+
+// START Unit test
 describe('GIVEN i am connected as an employee', () => {
   describe('WHEN i am on NewBill Page', () => {
     test('THEN the newBill page should be displayed', () => {
@@ -24,59 +30,77 @@ describe('GIVEN i am connected as an employee', () => {
       expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
     })
   })
+  describe("When I am on NewBill Page and I add a file", () => {
+    test("Then this new file should be modified in the input file", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage
+      });
+
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const inputFile = screen.getByTestId("file");
+
+      inputFile.addEventListener("change", handleChangeFile);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["image.jpg"], "image.jpg", { type: "image/jpg" })],
+        },
+      });
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(inputFile.files[0].name).toBe("image.jpg");
+    });
+  });
   describe('When I am on NewBill Page and I Submit form with a file containing a valid extension', () => {
     test('THEN I should be redirected to Bills page', () => {
       const html = NewBillUI()
       document.body.innerHTML = html
 
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-
       const newBill = new NewBill({
         document,
         onNavigate,
         firestore: null,
-        localStorage: window.localStorage,
+        localStorage: window.localStorage
       });
 
-      const handleSubmit = jest.fn(newBill.handleSubmit);
-      newBill.fileName = 'image.jpg';
+      const handleSubmit = jest.fn(newBill.handleSubmit)
+      newBill.fileName = 'image.jpg'
 
-      const submitBtnElt = screen.getByTestId('form-new-bill');
-      submitBtnElt.addEventListener('submit', handleSubmit);
-      fireEvent.submit(submitBtnElt);
+      const submitBtnElt = screen.getByTestId('form-new-bill')
+      submitBtnElt.addEventListener('submit', handleSubmit)
+      fireEvent.submit(submitBtnElt)
 
-      expect(handleSubmit).toHaveBeenCalled();
-      expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
+      expect(handleSubmit).toHaveBeenCalled()
+      expect(screen.getAllByText('Mes notes de frais')).toBeTruthy()
     })
   })
   describe('When I am on NewBill Page and I Submit form with a file containing a invalid extension', () => {
-    test('THEN I should stay on the new bill page and a error message should displayed', () => {
+    test('THEN I should stay on the NewBill page and a error message should be displayed', () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
 
       const newBill = new NewBill({
         document,
         onNavigate,
         firestore: null,
-        localStorage: window.localStorage,
+        localStorage: window.localStorage
       });
 
-      const handleSubmit = jest.fn(newBill.handleSubmit);
-      newBill.fileName = 'invalid';
+      const handleSubmit = jest.fn(newBill.handleSubmit)
+      newBill.fileName = 'invalid'
 
-      const submitBtnElt = screen.getByTestId('form-new-bill');
-      submitBtnElt.addEventListener('submit', handleSubmit);
-      fireEvent.submit(submitBtnElt);
+      const submitBtnElt = screen.getByTestId('form-new-bill')
+      submitBtnElt.addEventListener('submit', handleSubmit)
+      fireEvent.submit(submitBtnElt)
 
-      expect(handleSubmit).toHaveBeenCalled();
+      expect(handleSubmit).toHaveBeenCalled()
       expect(document.querySelector(".error-extension").style.display).toBe("block")
-      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy();
+      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
     })
   })
 })
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -22,33 +22,36 @@ export default class NewBill {
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
 
-    // add for checked file extension
-    const fileExtensionCheck = /(png|jpg|jpeg)/g;
-    const fileExtension = fileName.split(".").pop();
-    const fileMatchExtension = fileExtension.toLowerCase().match(fileExtensionCheck);
+    // add control for checked file extension
+    const fileExtensionCheck = /(png|jpg|jpeg)/g
+    const fileExtension = fileName.split(".").pop()
+    const fileMatchExtension = fileExtension.toLowerCase().match(fileExtensionCheck)
 
-    this.firestore
-      .storage
-      .ref(`justificatifs/${fileName}`)
-      .put(file)
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        this.fileUrl = url
-        // add 'invalid' in fileName URL if wrong extension
-        this.fileName = fileMatchExtension ? fileName : "invalid";
-      })
+    this.handleStorageFile(fileName, file, fileMatchExtension)
+  }
+
+  // add method for handle storage file
+  handleStorageFile = (fileName, file, fileMatchExtension) => {
+    if (this.firestore) {
+      this.firestore.storage
+        .ref(`justificatifs/${fileName}`)
+        .put(file)
+        .then((snapshot) => snapshot.ref.getDownloadURL())
+        .then((url) => {
+          this.fileUrl = url
+          this.fileName = fileMatchExtension ? fileName : "invalid"
+        })
+    }
   }
 
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    
-    // add for fileName extension error type on UI
+    // add UI information if fileName is invalid
     const inputFileExtension = document.querySelector(`input[data-testid="file"]`)
     if (this.fileName === "invalid") {
       inputFileExtension.classList.toggle('red-border')
       document.querySelector('.error-extension').style.display = 'block'
-      return;
+      return
     }
 
     const email = JSON.parse(localStorage.getItem("user")).email
